4b0f11882057eb64f5e3a2280075278271ee24eb
VIM-ROM <romero.acosta.vanessa.vra@gmail.com>
2025-04-18 22:05:14 -0600

    ¨inicio¨

---
+++ src/test/java/org/vim/pokeproject/PokeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vim.pokeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
